7ebc39b9fa7ae85cb7aeadd3c0717647c890d70b
x225franc <102668503+x225franc@users.noreply.github.com>
2022-09-30 09:27:28 +0100

    big_chungus-2

--- frontOffice/components/global/includes/header.php
+++ frontOffice/components/global/includes/header.php
@@ -1,1 +1,36 @@
-<h1>Questio</h1>
+<!DOCTYPE html>
+<html>
+    <head>
+        <menu id="menuH">
+            <li>
+                <title>Base connaisance</title>
+            </li>
+            <li>
+                <p><?php $text_message ?></p>
+            </li>
+            <li>
+                <div id="zone_utilisateur">
+                    <ul id="menuP">
+                        <?php
+                        if (is_null($idMenuP))
+                        {
+                          ?>
+                          <li><a>log in</a></li>
+                          <?php
+                        }
+                        else
+                        {
+                            ?>
+                            <li><a>profil</a></li>
+                            <li><a>deconection</a></li>
+                            <?php
+                        }
+                        ?>
+                        
+                    </ul>
+                    <img alt="image de profil">
+                </div>
+            </li>
+        </menu>
+    </head>
+</html>
