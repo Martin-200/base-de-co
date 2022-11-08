8762ef0302b9f7dae2488cee2eed9b1ab9a3e81b
AlexChabriel <100591867+AlexChabriel@users.noreply.github.com>
2022-11-08 10:52:07 +0100

    modification index et ajout image

--- frontOffice/index.php
+++ frontOffice/index.php
@@ -28,6 +28,8 @@
 
         /* global 92%+ browsers support */
         background: radial-gradient(circle at 100% 61%, rgba(215, 248, 240, 1) 0%, rgba(248, 246, 255, 1) 57%);
+
+       
     }
 
     .radiant-title {
@@ -43,14 +45,43 @@
         width: 100%;
         border: 1px solid #D3D5FF;
     }
-  .icone_comDIV
+    .float-container {
+    border: 3px none #fff;  
+    }
+    
+
+    .float-child {
+        width: 50%;
+        float: left;
+    }
+
+    .presentation
     {
-        width: 35;
+        padding-top: 125px;
+    }
+    .bouton
+    {
+        border-radius:31px;
+        background-color:#11053B;
+        color:#fff;
+        text-align: center;
+        padding: 6px 24px;
+        
+    }
+
+    .icone_Fic
+    {
+       height:55px;
     }
 
     .Capture_P
     {
         width: 60%;
+    }
+
+    .icone_com
+    {
+        height:55px;
     }
 
     .Cap_Comm
@@ -61,10 +92,62 @@
     {
         width: 60%;
     }
+
+    .communaute
+    {
+        padding-top: 125px;
+    }
+
+    .fiche
+    {
+        padding-top: 125px;
+    }
+
+    .tecno
+    {
+        padding-top: 125px;
+        padding-bottom: 75px;
+    }
+    .icon_lang
+    {
+        padding-top: 95px;
+    }
+    .aventage
+    {
+        padding-top: 85px;
+        background-color:#11053B;
+        color:#fff;
+        padding-bottom: 85px;
+    }
+
+    .title_center
+    {
+        color: #fff;
+    }
+
+    .titreBico
+    {
+        color: #11053B;
+        font-weight: 700;
+        text-decoration: none;
+        
+    }
+    a.titreBico:hover
+    {
+        color: #11053B;
+    }
+    .lien_B
+    {
+        text-decoration: none;
+        color:#fff;
+    }
+
+   
+   
 </style>
 
 <body>
-    <section class="hero-header">
+    <section id="recherche" class="hero-header">
 
         <div class="container">
             <div class="row">
@@ -74,7 +157,7 @@
                             <p>Base de connaissance</p>
                         </div>
                         <H1 class="radiant-title">Jamais 4 Erreurs <br> Sans 4 Solutions</H1>
-                        <h2>Trouve la solution a ton erreur</h2>
+                        <h6>Trouve la solution a ton erreur</h6>
                         <form action="./pages/home/search_action.php" method="POST">
                             <input type="search" name="searchProblem" id="searchProblem" placeholder='Rechercher'>
                         </form>
@@ -90,20 +173,21 @@
     <section class="presentation">
             <div class="row">
                 <div class="presentation_div1 col d-flex align-items-center justify-content-center">
-                    <div>
-                        <div class="mini-title">
-                            <p>Présentation</p>
+                    <div class="pre col d-flex align-items-center justify-content-center">
+                        <div>
+                            <div class="mini-title">
+                                <p>Présentation</p>
+                            </div>
+                            <H1 class="title">Une base de <br> connaissances <br> communautaire </H1>
+                            <h6> Réalisé  <a class="titreBico"> pour vous </a> et <a class="titreBico"> par vous </a> </h6>
+                            <button class="bouton" type="button"><a class="lien_B" href="#recherche">Rechercher mon erreur</a></button>
                         </div>
-                        <H1 class="title">Une base de <br> connaissances <br> communautaire </H1>
-                        <h2>Réalisé pour vous et par vous</h2>
-                        <button><a href="hero-header">Rechercher mon erreur</a></button>
+                    </div>
+                    <div class="presentation_div2 col d-flex align-items-center justify-content-center">
+                        <img class="Capture_P" src="../ressource/capureFiche.png" alt="Capture fiche ereur avec comentaire">
                     </div>
                 </div>
-                    
-                <div class="presentation_div2 col d-flex align-items-center justify-content-center">
-                    <img class="Capture_P" src="../ressource/capureFiche.png" alt="Capture fiche ereur avec comentaire">
-                </div>
-        </div>
+            </div>
 
     </section>
 
@@ -118,12 +202,10 @@
                             <p>Communauté</p>
                         </div>
                         <H1 class="title">Espace <br>  commentaire </H1>
-                        <h4>404.io vous aide  à resoudre l’ensemble de vos <br> problèmes. Possédant une grande base de <br> connaissances notamment alimentée par vous,<br> nous vous proposons un maximum de solutions. </h4>
-                        <div class="row">
-                            <div class="icone_comDIV col">
+                        <h6>404.io vous aide  à resoudre l’ensemble de vos <br> problèmes. Possédant une grande base de <br> connaissances notamment alimentée par vous,<br> nous vous proposons un maximum de solutions. </h6>
+                        <div>
+                            <div class="icone_comDIV d-flex">
                                 <img class="icone_com" src="../ressource/Frame.png" alt="icone_com">
-                            </div>
-                            <div class="text_comDIV col">
                                 <p>Nous mettons  à votre disposition un espace commentaire  - <br> présent  sous chaque fiche réponse. Cet espace  vous permettra <br> d’échanger avec la communauté.</p>
                             </div>
                         </div>
@@ -140,13 +222,11 @@
                             <p>Fiche</p>
                         </div>
                         <H1 class="title">Proposer votre <br>  fiche réponse </H1>
-                        <h4>Notre base vous aide à trouver les solutions à vos <br> problèmes, de plus elle tend à s’agrandir. Ainsi, via la <br> proposition de fiches, vous nous aiderez à faire <br> évoluer le site. </h4>
-                        <div class="row">
-                            <div class="icone_comDIV col">
+                        <h6>Notre base vous aide à trouver les solutions à vos <br> problèmes, de plus elle tend à s’agrandir. Ainsi, via la <br> proposition de fiches, vous nous aiderez à faire <br> évoluer le site. </h6>
+                        <div>
+                            <div class="icone_comDIV d-flex">
                                 <img class="icone_Fic" src="../ressource/Icon_Ed.png" alt="icone_Fiche">
-                            </div>
-                            <div class="text_comDIV col" >
-                                <p>En plus de la proposition de fiche, 404.io vous permet de <br> modifier des solutions existantes - si vous les trouvez <br> incomplètes ou erronées.</p>
+                                <p class="ms-3">En plus de la proposition de fiche, 404.io vous permet de <br> modifier des solutions existantes - si vous les trouvez <br> incomplètes ou erronées.</p>
                             </div>
                         </div>
                     </div>              
@@ -158,27 +238,30 @@
             </div>
     </section>
 
-    <section class="tecno">
-        <div class="Tecno_contenu">
-            <div class="mini-title">
+    <section class="tecno ">
+        <div class="Tecno_contenu d-flex align-items-center justify-content-center flex-column" >
+            <div class="mini-title ">
                 <p>Technologies</p>
             </div>
-            <H1 class="title_center">Exemples de technologies traitées</H1>
-            <img src="../ressource/icon_dev.png">
+            <H1 class="title">Exemples de technologies traitées</H1>
+            <img class="icon_lang " src="../ressource/icon_dev.png">
         </div>
     </section>
 
     <section class="aventage">
         <div class="aventage_contenu">
-        <div class="mini-title">
-                <p>Avantage</p>
+            <div class="aligne_titre d-flex align-items-center justify-content-center flex-column">
+                <div class="mini-title">
+                    <p>Avantage</p>
+                </div>
+                <H1 class="title_center">Avantages</H1>
             </div>
-            <H1 class="title_center">Avantages</H1>
-            <div class="col d-flex align-items-center">
+            
+            <div class="col d-flex align-items-center justify-content-center">
                 <div class="aventage_div1">
-                    </div class="row">
+                    <div class="row">
                         <div class="icone_aven">
-                            <img alt="icone_lign1_col1">
+                            <img alt="icone_lign1_col1" src="../ressource/recueil-icon.png">
                         </div>
                         <div class="text_aven">
                             <h4>Un recueil de fiches en expansion</h4>
@@ -187,7 +270,7 @@
                     </div>
                     <div class="row">
                         <div class="icone_aven">
-                            <img alt="icone_lign2_col1">
+                            <img alt="icone_lign2_col1" src="../ressource/commu-icon.png">
                         </div>
                         <div class="text_aven">
                             <h4>Une communauté soudée et passionnée</h4>
@@ -196,19 +279,20 @@
                     </div>
                     <div class="row">
                         <div class="icone_aven">
-                            <img alt="icone_lign3_col1">
+                            <img alt="icone_lign3_col1" src="../ressource/acces-icon.png">
                         </div>
                         <div class="text_aven">
                             <h4>Un accès cross-plateforme</h4>
                             <p>Accèdez à la base de connaissances où que vous soyez, peu <br> importe l’appareil</p>
                         </div>
                     </div>
+                    
                 </div>
 
-                <div class="aventage_div2">
+                <div class="aventage_div2 ">
                     <div class="row">
                         <div class="icone_aven">
-                            <img alt="icone_lign1_col2">
+                            <img alt="icone_lign1_col2" src="../ressource/verif-icon.png">
                         </div>
                         <div class="text_aven">
                             <h4>Vérification par une équipe passionnée</h4>
@@ -217,7 +301,7 @@
                     </div>
                     <div class="row">
                         <div class="icone_aven">
-                            <img alt="icone_lign2_col2">
+                            <img alt="icone_lign2_col2" src="../ressource/startup-icon.png">
                         </div>
                         <div class="text_aven">
                             <h4>Startup unique en son genre</h4>
@@ -226,7 +310,7 @@
                     </div>
                     <div class="row">
                         <div class="icone_aven">
-                            <img alt="icone_lign3_col2">
+                            <img alt="icone_lign3_col2" src="../ressource/dev-icon.png">
                         </div>
                         <div class="text_aven">
                             <h4>Des développeurs rigoureux</h4>
